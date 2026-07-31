6260687b03a3c32a875d19c901406606a833e96a
Gonhex <goncalosilva.silva0@gmail.com>
2026-07-31 18:20:52 +0100

    Script para criar bases de dados

--- sql/001_criar_bd.sql
+++ sql/001_criar_bd.sql
@@ -87,3 +87,17 @@ nivel INT NOT NULL
 ativo TINYINT NOT NULL DEFAULT 1
 	CHECK (ativo IN (0, 1))
     );
+
+
+-- Script para ligar as bases de dados --
+
+use techservice_equipa4;
+
+ALTER TABLE techservice_equipa4.ordens_servico
+ADD CONSTRAINT fk_ordens_servico_prioridade_os
+FOREIGN KEY (id_prioridade)
+REFERENCES techservice_equipa4.prioridade_os(id_prioridade)
+ON DELETE SET NULL
+ON UPDATE CASCADE;
+
+----------------------------------------------------------
