6767c18b8338b10fc7e3a4b49d703eaf684e8650
PDBL <pedro.db.lourenco@gmail.com>
2026-07-31 18:22:16 +0100

    Alterar conexao

--- src/database/conexao.py
+++ src/database/conexao.py
@@ -9,9 +9,9 @@ load_dotenv()
 
 def conectar():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST", "aulaslab.com"),
         port=int(os.getenv("DB_PORT", "3306")),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", ""),
-        database=os.getenv("DB_NAME", "techservice_db")
+        user=os.getenv("DB_USER", "Techservice"),
+        password=os.getenv("DB_PASSWORD", "TechService@2026!"),
+        database=os.getenv("DB_NAME", "techservice_equipa4")
     )
