35f0eab279ba950f6e1c94c9c36d0ac8717f6a93
Mário_Fortunato_808 <theberserker815@gmail.com>
2026-08-03 14:37:30 +0100

    conexão efetuada

--- src/database/conexao.py
+++ src/database/conexao.py
@@ -11,7 +11,7 @@ def conectar():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST", "162.240.171.8"),
         port=int(os.getenv("DB_PORT", "3306")),
-        user=os.getenv("DB_USER", "Techservice"),
+        user=os.getenv("DB_USER", "techservice"),
         password=os.getenv("DB_PASSWORD", "TechService@2026!"),
         database=os.getenv("DB_NAME", "techservice_equipa4")
     )
